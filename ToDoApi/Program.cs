6fe37a1e2a8efaa2b72ef18d858d92234b584f10
omsalekar <omsalekar6052@gmil.com>
2025-12-26 13:42:31 +0530

    Preparing for hosting ToDoApi on Render

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -15,6 +15,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
